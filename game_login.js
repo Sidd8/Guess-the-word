5d67fde7ea24dfb6594e45ce89c590ac3811cdf2
Sidd8 <72198957+Sidd8@users.noreply.github.com>
2021-01-16 11:56:56 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+function player_login(){
+    var player1=document.getElementById("player1_name").value;
+    var player2=document.getElementById("player2_name").value;
+    localStorage.setItem("player1",player1);
+    localStorage.setItem("player2",player2);
+    window.location="game_page.html";
+}
